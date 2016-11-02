47ea02d953cbfec4a57250df7cf51cf17173021c
Raghav <ragz2203@gmail.com>
2016-11-02 22:34:08 +0530

    Mail Sending Part Done
    A mail is sent to the user mail id regarding registration. Acheived
    using JavaMail API

---
+++ app/src/main/java/com/ragz/myregistration/Config.java
@@ -0,0 +1,12 @@
+package com.ragz.myregistration;
+
+/**
+ * Created by Ragz on 02-Nov-16.
+ */
+
+public class Config extends javax.mail.Authenticator {
+
+    public static final String EMAIL ="zukathar@gmail.com";
+    public static final String PASSWORD ="firenation";
+
+}
